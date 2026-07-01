a1ce86c7684a84f93c9867f00d9d04a10a9583d9
alexbalak21 <alex.balak@outlook.com>
2026-07-01 14:55:07 +0200

    imported lucid react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Shirt } from "lucide-react";
+
 export default function App() {
   return (
     <div>
-
-      <h1 className="text-3xl font-bold">
-        Hello world!
+      <h1 className="text-3xl font-bold inline-flex items-center gap-2">
+        <Shirt className="size-8" /> Shop App
       </h1>
     </div>
-  )
+  );
 }
